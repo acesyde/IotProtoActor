81ca73922e7e9a2bf0872803219957c812806e53
acesyde <acesyde@gmail.com>
2022-03-24 14:00:11 +0100

    cluster / endpoint

---
+++ src/IotProto.Host/Endpoints/Hello/HelloEndpoint.cs
@@ -0,0 +1,18 @@
+﻿using FastEndpoints;
+
+namespace IotProto.Host.Endpoints.Hello;
+
+public class HelloEndpoint : EndpointWithoutRequest
+{
+    public override void Configure()
+    {
+        Verbs(Http.GET);
+        Routes("/api/hello");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        await this.SendAsync(new { }, StatusCodes.Status200OK, ct);
+    }
+}
---
+++ src/IotProto.Host/Extensions/ActorSystemExtensions.cs
@@ -0,0 +1,46 @@
+﻿using IotProto.Host.Services;
+using Proto;
+using Proto.Cluster;
+using Proto.Cluster.Partition;
+using Proto.Cluster.Testing;
+using Proto.DependencyInjection;
+using Proto.Remote.GrpcNet;
+
+namespace IotProto.Host.Extensions;
+
+public static class ActorSystemExtensions
+{
+    public static void AddActorSystem(this IServiceCollection services)
+    {
+        services.AddSingleton(provider =>
+        {
+            // actor system configuration
+
+            var actorSystemConfig = ActorSystemConfig
+                .Setup();
+
+            // remote configuration
+
+            var remoteConfig = GrpcNetRemoteConfig
+                .BindToLocalhost();
+
+            // cluster configuration
+
+            var clusterConfig = ClusterConfig
+                .Setup(
+                    clusterName: "ProtoClusterTutorial",
+                    clusterProvider: new TestProvider(new TestProviderOptions(), new InMemAgent()),
+                    identityLookup: new PartitionIdentityLookup()
+                );
+
+            // create the actor system
+
+            return new ActorSystem(actorSystemConfig)
+                .WithServiceProvider(provider)
+                .WithRemote(remoteConfig)
+                .WithCluster(clusterConfig);
+        });
+
+        services.AddHostedService<ActorSystemClusterHostedService>();
+    }
+}
